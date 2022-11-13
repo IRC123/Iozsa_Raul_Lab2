c46cf025816b3a7dc2f49018f617624430df8be6
iozsa <iozsa@LAPTOP-TH9IPASF>
2022-11-13 21:23:05 +0200

    Add project files.

---
+++ Data/Iozsa_Raul_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Iozsa_Raul_Lab2.Models;
+
+namespace Iozsa_Raul_Lab2.Data
+{
+    public class Iozsa_Raul_Lab2Context : DbContext
+    {
+        public Iozsa_Raul_Lab2Context (DbContextOptions<Iozsa_Raul_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Iozsa_Raul_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Iozsa_Raul_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Iozsa_Raul_Lab2.Data;
+using Iozsa_Raul_Lab2.Models;
+
+namespace Iozsa_Raul_Lab2.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Iozsa_Raul_Lab2.Data.Iozsa_Raul_Lab2Context _context;
+
+        public IndexModel(Iozsa_Raul_Lab2.Data.Iozsa_Raul_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Publisher != null)
+            {
+                Publisher = await _context.Publisher.ToListAsync();
+            }
+        }
+    }
+}
